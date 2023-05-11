b12ca5f00f577f58db2812b127f13f0fcc8f2788
geumjin99 <71214437+geumjin99@users.noreply.github.com>
2023-05-11 10:47:56 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='PyRINEX',
+    packages=find_packages(),
+    version='0.1.0',
+    description='RINEX preprocessing package',
+    author='Me',
+    license='MIT',
+)
