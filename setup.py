22e39d6614f5670da81b2d3f6901392a79cd7599
geumjin99 <hanjinzhen9@gmail.com>
2023-12-07 18:02:30 +0900

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyRINEX',
-    version='3.0',
+    version='3.0.1',
     author='Han Jinzhen',
     author_email='hanjinzhen9@gmail.com',
 
@@ -13,10 +13,7 @@ setup(
         "License :: OSI Approved :: Apache Software License 2.0",
     ],
     install_requires=[
-        'numpy==1.21.1',
-        'matplotlib==3.4.1',
         'chardet==4.0.0',
-        'panda ==1.21.1',
         'seaborn==0.13.0'
     ],
     python_requires='>=3.6',
